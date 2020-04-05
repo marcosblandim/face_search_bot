fd336ff8a09ff03bc9e7f9d0896ff15be1eee30b
marcosblandim <marcosblandim@gmail.com>
2020-04-05 04:50:35 -0300

    .

--- visualize_pickle.py
+++
@@ -1,6 +0,0 @@
-import pickle
-from pprint import pprint
-with open("encodings.pickle",'rb') as f:
-    # print(pickle.loads(f.read()))
-    # print(pickle.loads(f.read())[0])
-    pprint(pickle.loads(f.read())[1])
